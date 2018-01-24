5dff6a19a5865e07c86133d72350e6dc6a730a26
RandyCheng <linusche@gmail.com>
2018-01-24 10:46:46 +0800

    post request.query

---
+++ AjaxSite/Create.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Create : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //使用者透過http post傳過來的資料
+        //在server端用 Request.Form["欄位名稱"]來接受資料
+
+        //使用者透過http get傳過來的資料
+        //在server端 Request.QueryString["參數名稱"]
+        string strName = Request.QueryString["username"];
+        string strPassword = Request.QueryString["password"];
+        string strEmail = Request.QueryString["email"];
+
+    }
+}
---
+++ AjaxSite/add.ashx
@@ -0,0 +1,21 @@
+﻿<%@ WebHandler Language="C#" Class="add" %>
+
+using System;
+using System.Web;
+
+public class add : IHttpHandler {
+
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "text/plain";
+        context.Response.Write("Hello World");
+        context.Response.Write(context);
+
+    }
+
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
